6efe44d708517db1e191e22de940b16b7ea2fba3
NazarkoNaN <nazar.ivanyuk@gmail.com>
2023-06-14 22:44:30 -0700

    first one

---
+++ blog/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.conf import settings
+from django.utils import timezone
+
+class Post(models.Model):
+    autor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField
+    creted_date = models.DateField(default=timezone.now)
+    published = models.BooleanField(blank=True, default=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
