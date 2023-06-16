40fab2054aada2226c499530d84683d0fe05a18c
NazarkoNaN <nazar.ivanyuk@gmail.com>
2023-06-16 02:10:08 -0700

    static

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import *
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('startpage', startpage, name='startpage'),
     path('post_list', post_list, name='post_list'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
